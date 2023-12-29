040ed82d3c72064767997657aa302d155b279a13
Mallika <mallikapathak36@gmail.com>
2023-12-29 15:47:04 +0530

    add

---
+++ temp/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+  <>
+  <Navbar/>
+  <p>lorem20</p>
+  </>
+  );
+}
+
+export default App;
